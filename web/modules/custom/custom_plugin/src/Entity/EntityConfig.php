a1868b219932bb26ae87ea6876a42157c840da58
nomiso2000 <nomiso.study@gmail.com>
2022-06-20 18:19:09 +0300

    fixed wrong name for entity config

---
+++ web/modules/custom/custom_plugin/src/Entity/EntityConfig.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\custom_plugin\Entity;
+
+use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\custom_plugin\EntityConfigInterface;
+
+/**
+ * Defines the entity_config entity type.
+ *
+ * @ConfigEntityType(
+ *   id = "entity_config",
+ *   label = @Translation("entity_config"),
+ *   label_collection = @Translation("entity_configs"),
+ *   label_singular = @Translation("entity_config"),
+ *   label_plural = @Translation("entity_configs"),
+ *   label_count = @PluralTranslation(
+ *     singular = "@count entity_config",
+ *     plural = "@count entity_configs",
+ *   ),
+ *   handlers = {
+ *     "list_builder" = "Drupal\custom_plugin\EntityConfigListBuilder",
+ *     "form" = {
+ *       "add" = "Drupal\custom_plugin\Form\EntityConfigForm",
+ *       "edit" = "Drupal\custom_plugin\Form\EntityConfigForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm"
+ *     }
+ *   },
+ *   config_prefix = "entity_config",
+ *   admin_permission = "administer entity_config",
+ *   links = {
+ *     "collection" = "/admin/structure/entity-config",
+ *     "add-form" = "/admin/structure/entity-config/add",
+ *     "edit-form" = "/admin/structure/entity-config/{entity_config}",
+ *     "delete-form" = "/admin/structure/entity-config/{entity_config}/delete"
+ *   },
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "label",
+ *     "uuid" = "uuid"
+ *   },
+ *   config_export = {
+ *     "id",
+ *     "label",
+ *     "description"
+ *   }
+ * )
+ */
+class EntityConfig extends ConfigEntityBase implements EntityConfigInterface {
+
+  /**
+   * The entity_config ID.
+   *
+   * @var string
+   */
+  protected $id;
+
+  /**
+   * The entity_config label.
+   *
+   * @var string
+   */
+  protected $label;
+
+  /**
+   * The entity_config status.
+   *
+   * @var bool
+   */
+  protected $status;
+
+  /**
+   * The entity_config description.
+   *
+   * @var string
+   */
+  protected $description;
+
+}
---
+++ web/modules/custom/custom_plugin/src/EntityConfigInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\custom_plugin;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+ * Provides an interface defining an entity_config entity type.
+ */
+interface EntityConfigInterface extends ConfigEntityInterface {
+
+}
